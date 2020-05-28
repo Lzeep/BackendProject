58be40d530bd7e79aedaecd2cbb2c6e41a014987
Azret <azret1jusuev@gmail.com>
2020-05-28 16:35:59 +0600

    last commit

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -10,7 +10,7 @@
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
 
-
+sdf
 </head>
 <body>
 <h1>{{ $company->title  }}</h1>
